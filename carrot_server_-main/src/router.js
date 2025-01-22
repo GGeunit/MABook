aa8c8d9c5962eb229658994480a65d8e0af70dbd
GGeunit <kie_love@naver.com>
2025-01-22 19:43:51 +0900

    Node.js 설정

--- carrot_server_-main/src/router.js
+++ carrot_server_-main/src/router.js
@@ -6,20 +6,22 @@ const upload = multer({ dest: 'storage/' });
 const authenticateToken = require('./middleware/authenticate');
 
 const webController = require('./web/controller');
-const categoryController = require('./category/controller');
-const expenseController = require('./expense/controller');
-const userController = require('./user/controller');
+const categoryController = require('./api/category/controller');
+const expenseController = require('./api/expense/controller');
+const userController = require('./api/user/controller');
 // const fileController = require('./api/file/controller');
 
 const { logRequestTime } = require('./middleware/log');
 
+router.use(logRequestTime);
 
 router.get('/', webController.home);
 router.get('/page/:route', logRequestTime, webController.page);
 
 router.get('/category', categoryController.show);
 
-router.use(logRequestTime);
+router.post('/user/register', userController.register);
+router.post('/user/login', userController.login);
 
 // router.post('/file', upload.single('file'), fileController.upload);
 // router.get('/file/:id', fileController.download);
@@ -27,8 +29,7 @@ router.use(logRequestTime);
 
 // router.post('/auth/phone', apiUserController.phone);
 // router.put('/auth/phone', apiUserController.phoneVerify);
-router.post('/user/register', userController.register);
-router.post('/user/login', userController.login);
+
 
 router.use(authenticateToken);
 
